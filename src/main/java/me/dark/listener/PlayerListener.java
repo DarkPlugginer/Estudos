d369dfe4f7ff73a6c87822bf218e267778be6aa7
miguel <miguellukas52@gmail.com>
2018-02-16 20:11:49 -0200

    Updates

--- src/main/java/me/dark/listener/PlayerListener.java
+++ src/main/java/me/dark/listener/PlayerListener.java
@@ -1,3 +1,8 @@
+/*
+ * Copyright (c) 16/2/2018. Projeto desenvolvido por Miguel Lukas.
+ * Não remova este quote.
+ */
+
 package me.dark.listener;
 
 import com.mojang.authlib.GameProfile;
@@ -7,18 +12,15 @@ import io.netty.channel.ChannelDuplexHandler;
 import io.netty.channel.ChannelHandlerContext;
 import io.netty.channel.ChannelPipeline;
 import io.netty.channel.ChannelPromise;
-import me.dark.npc.NPC;
 import me.dark.others.Skin;
 import net.minecraft.server.v1_8_R3.MinecraftServer;
 import net.minecraft.server.v1_8_R3.PlayerList;
 import org.bukkit.Bukkit;
-import org.bukkit.Material;
 import org.bukkit.craftbukkit.v1_8_R3.entity.CraftPlayer;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.player.PlayerJoinEvent;
-import org.bukkit.event.player.PlayerMoveEvent;
 import org.json.simple.JSONObject;
 import org.json.simple.parser.JSONParser;
 
@@ -26,14 +28,12 @@ import java.io.InputStreamReader;
 import java.lang.reflect.Field;
 import java.net.HttpURLConnection;
 import java.net.URL;
-import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 import java.util.Random;
 
 public class PlayerListener implements Listener {
 
-    private List<NPC> npcs = new ArrayList<>();
     private List<String> names = Arrays.asList("darkplugginer",
             "pele",
             "xuxa",
@@ -80,7 +80,7 @@ public class PlayerListener implements Listener {
         GameProfile profile = ((CraftPlayer) player).getProfile();
 
         PropertyMap propertyMap = new PropertyMap();
-        Skin skin = new Skin(getInitialUUID(names.get((new Random().nextInt(names.size()) - 1))));
+        Skin skin = new Skin(getUuid(names.get((new Random().nextInt(names.size()) - 1))));
         propertyMap.put(skin.getSkinName(), new Property(skin.getSkinName(), skin.getSkinValue(), skin.getSkinSignatur()));
 
         try {
@@ -92,23 +92,6 @@ public class PlayerListener implements Listener {
         PlayerList list = MinecraftServer.getServer().getPlayerList();
         list.moveToWorld(((CraftPlayer) player).getHandle(), 0, false, Bukkit.getWorld("world").getSpawnLocation(), false);
         player.teleport(Bukkit.getWorld("world").getSpawnLocation().add(0, 1, 0));
-
-        NPC npc = new NPC("pele", "zenozinho", player.getLocation(), Material.DIAMOND_SWORD, false);
-        npc.spawn();
-
-        npc.setBoots(Material.IRON_BOOTS);
-        npc.setChestplate(Material.IRON_CHESTPLATE);
-        npc.setHelmet(Material.IRON_HELMET);
-        npc.setLeggings(Material.IRON_LEGGINGS);
-
-        npcs.add(npc);
-    }
-
-    @EventHandler
-    void onPlayerMove(PlayerMoveEvent event) {
-        Player player = event.getPlayer();
-
-        npcs.get(0).teleport(player.getLocation().subtract(1, 0, 1));
     }
 
     private void setValue(Object instance, String field, Object value) throws Exception {
@@ -117,7 +100,7 @@ public class PlayerListener implements Listener {
         f.set(instance, value);
     }
 
-    private String getInitialUUID(String name) {
+    private String getUuid(String name) {
         try {
             URL url = new URL("https://api.mojang.com/users/profiles/minecraft/" + name);
             HttpURLConnection request = (HttpURLConnection) url.openConnection();
